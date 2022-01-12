ebcb8e77145f8c060005eed627b195ffdf27090d
Leandro de Araujo Lopes <tecnolopes@hotmail.com>
2022-01-12 11:54:00 -0300

    Atende aos requisitos 19 a 21

--- script.js
+++ script.js
@@ -28,3 +28,38 @@ function testSubmit(event) {
 }
 
 agreementCheckBox.addEventListener('click', testSubmit)
+
+const counter = document.getElementById('textarea');
+
+function testTextarea(event) {
+    const quant = event.target.value.length;
+    const quant2 = document.getElementById('counter');
+    quant2.innerText = 500 - quant;
+}
+
+counter.addEventListener('keyup', testTextarea);
+
+const nome = document.getElementById('input-name');
+const sobrenome = document.getElementById('input-lastname');
+const email2 = document.getElementById('input-email');
+const casa = document.getElementById('house');
+const familia = document.getElementById('label-family');
+const materias = document.getElementById('label-content');
+const avaliacao = document.getElementById('label-rate-form');
+const observacoes = document.getElementById('textarea');
+
+const nomeli = document.getElementById('nome-li');
+const emailli = document.getElementById('email-li');
+const casali = document.getElementById('casa-li');
+const familiali = document.getElementById('familia-li');
+const materiasli = document.getElementById('materias-li');
+const avaliacaoli = document.getElementById('avaliacao-li');
+const observacoesli = document.getElementById('observacoes-li');
+
+function testContent(event){
+    event.preventDefault();
+    nomeli.innerText = 'Nome: ' + nome.value + ' ' + sobrenome.value;
+}
+
+const buttonSubmit2 = document.getElementById('submit-btn');
+buttonSubmit2.addEventListener('click', testContent);
