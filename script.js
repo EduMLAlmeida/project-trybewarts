eba5c7827a554c264f640276e8bea5b71ce6ba82
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-13 10:56:04 -0300

    Atende ao requisito 21 e testlint

--- script.js
+++ script.js
@@ -3,38 +3,35 @@ const password = document.getElementById('password');
 const formButton = document.getElementById('form-button');
 
 function testEmail() {
-    const emailValue = email.value;
-    const passwordValue = password.value;
-
-    if(email.value === 'tryber@teste.com' && password.value === '123456') {
-        window.alert('Olá, Tryber!')
-    } else {
-        window.alert('Email ou senha inválidos.')
-    };
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
 }
 
-formButton.addEventListener('click', testEmail)
+formButton.addEventListener('click', testEmail);
 
 const agreementCheckBox = document.getElementById('agreement');
 const buttonSubmit = document.getElementById('submit-btn');
 buttonSubmit.disabled = true;
 
-function testSubmit(event) {
-    if (buttonSubmit.disabled === true) {
-        buttonSubmit.disabled = false;
-    } else if (buttonSubmit.disabled === false) {
-        buttonSubmit.disabled = true
-    }
+function testSubmit() {
+  if (buttonSubmit.disabled === true) {
+    buttonSubmit.disabled = false;
+  } else if (buttonSubmit.disabled === false) {
+    buttonSubmit.disabled = true;
+  }
 }
 
-agreementCheckBox.addEventListener('click', testSubmit)
+agreementCheckBox.addEventListener('click', testSubmit);
 
 const counter = document.getElementById('textarea');
 
 function testTextarea(event) {
-    const quant = event.target.value.length;
-    const quant2 = document.getElementById('counter');
-    quant2.innerText = 500 - quant;
+  const quant = event.target.value.length;
+  const quant2 = document.getElementById('counter');
+  quant2.innerText = 500 - quant;
 }
 
 counter.addEventListener('keyup', testTextarea);
@@ -43,9 +40,9 @@ const nome = document.getElementById('input-name');
 const sobrenome = document.getElementById('input-lastname');
 const email2 = document.getElementById('input-email');
 const casa = document.getElementById('house');
-const familia = document.getElementById('label-family');
-const materias = document.getElementById('label-content');
-const avaliacao = document.getElementById('label-rate-form');
+const familia = document.querySelectorAll('.family-radio');
+const materias = document.querySelectorAll('.subject');
+const avaliacao = document.querySelectorAll('.rate-radio');
 const observacoes = document.getElementById('textarea');
 
 const nomeli = document.getElementById('nome-li');
@@ -56,10 +53,38 @@ const materiasli = document.getElementById('materias-li');
 const avaliacaoli = document.getElementById('avaliacao-li');
 const observacoesli = document.getElementById('observacoes-li');
 
-function testContent(event){
-    event.preventDefault();
-    nomeli.innerText = 'Nome: ' + nome.value + ' ' + sobrenome.value;
+function testContent(event) {
+  event.preventDefault();
+  nomeli.innerText = `Nome: ${nome.value} ${sobrenome.value}`;
+  emailli.innerText = `Email: ${email2.value}`;
+  casali.innerText = `Casa: ${casa.value}`;
+  for (let index = 0; index < familia.length; index += 1) {
+    if (familia[index].checked) {
+      familiali.innerText = `Família: ${familia[index].value}`;
+    }
+  }
+}
+
+function testContent2() {
+  const arrayMaterias = [];
+  for (let index2 = 0; index2 < materias.length; index2 += 1) {
+    if (materias[index2].checked) {
+      arrayMaterias.push(` ${materias[index2].value}`);
+    }
+  }
+  materiasli.innerText = `Matérias: ${arrayMaterias.toString()}`;
+}
+
+function testContent3() {
+  for (let index3 = 0; index3 < avaliacao.length; index3 += 1) {
+    if (avaliacao[index3].checked) {
+      avaliacaoli.innerText = `Avaliação: ${avaliacao[index3].value}`;
+    }
+  }
+  observacoesli.innerText = `Observações: ${observacoes.value}`;
 }
 
 const buttonSubmit2 = document.getElementById('submit-btn');
 buttonSubmit2.addEventListener('click', testContent);
+buttonSubmit2.addEventListener('click', testContent2);
+buttonSubmit2.addEventListener('click', testContent3);
